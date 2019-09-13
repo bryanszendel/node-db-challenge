76e33524ac239ae2af8eee6ad6b5ba70bf13b869
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-13 09:57:50 -0600

    projects migration file created

---
+++ data/migrations/20190913094044_projects.js
@@ -0,0 +1,46 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable()
+      tbl.text('description', 500)
+      tbl.boolean('completed', ).defaultTo('false').notNullable()
+    })
+    .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.text('description', 500).notNullable()
+      tbl.text('notes', 500)
+      tbl.boolean('completed').defaultTo('false').notNullable()
+    })
+    .createTable('resources', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable().unique()
+      tbl.text('description', 500)
+    })
+    .createTable('project_resources', tbl => {
+      tbl.increments();
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      tbl
+        .integer('resource_id')
+        .unsigned()
+        .references('id')
+        .inTable('resources')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
+};
